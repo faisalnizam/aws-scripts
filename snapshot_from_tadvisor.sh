8b0b5ede1a02b341ede31d1ccd2d6ffa6d6e7a7b
APPLE <apple@APPLEs-MacBook-Pro.local>
2019-09-06 21:45:03 +0400

    ADD User Input Option

--- snapshot_from_tadvisor.sh
+++ snapshot_from_tadvisor.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 # INPUT REQUIRED 
 # REGION NAME AS ARGUMENT 1
 # PROFILE NAE FROM CREDENTIALS FILE ARGUMENT 2
@@ -12,7 +12,13 @@ input=$3
  while IFS= read -r line
    do
       volume_id=${line}
-      echo "Orphan Volume:   "$volume_id 
-      echo "Creating Snapshot of $volume_id"  
-#aws --profile ${profile} ec2 create-snapshot --volume-id {$volume_id} --description 'Orphan Volume Snapshot' --tag-specifications 'ResourceType=snapshot,Tags=[{Key=env,Value=prod},{Key=orphan,Value=yes}]'
+      echo "Orphan Volume:  ${volume_id}"
+      echo "Creating Snapshot of ${volume_id}"
+      #aws --profile ${profile} ec2 create-snapshot --volume-id {$volume_id} --description 'Orphan Volume Snapshot' --tag-specifications 'ResourceType=snapshot,Tags=[{Key=env,Value=prod},{Key=orphan,Value=yes}]'
+      read -p "Run command $answer? [yn]" answer </dev/tty
+      if [ "${answer}" = "y" ]; then
+         echo " Continue Backing Up"
+      else 
+         exit 0 
+      fi 
     done < "$input"
