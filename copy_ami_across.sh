f76f6c8b8a8d8e3383320a5f9139e558d3dcacea
APPLE <apple@APPLEs-MacBook-Pro.local>
2019-09-08 10:08:34 +0400

    Copy AMI-IDS Across Regions

--- copy_ami_across.sh
+++ copy_ami_across.sh
@@ -1,5 +1,7 @@
 #! /bin/bash 
 
+echo "Enter Profile to Use: " 
+read profile
 
 echo "Enter Region To Copy From: " 
 read region_from 
@@ -17,6 +19,6 @@ read ami_to_copy
 for ami_id in $(echo ${ami_to_copy} | sed "s/,/ /g")
 do
     echo "Copying AMI ${ami_id} from ${region_fron} to ${region_to} "
-    #aws --profile ${region_from} copy-image --source-region ${region_to} --region {} --source-ami-id ${ami_id}
+    aws --profile ${profile} ec2 copy-image --source-region ${region_from} --region ${region_to} --source-image-id ${ami_id} --name ${ami_id}
 done
 
